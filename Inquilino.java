65ff3623472f2f6e50814cd9f23c2c4a057dda05
Felipe Assis <80302392+felipefassis97@users.noreply.github.com>
2025-10-22 14:40:46 -0300

    Update Inquilino.java

---
+++ Inquilino.java
@@ -0,0 +1,20 @@
+class Inquilino {
+    private String nome;
+    private String telefone;
+    private String cpf;
+
+    public Inquilino(String nome, String telefone, String cpf) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.cpf = cpf;
+    }
+
+    public String getNome() { return nome; }
+    public String getTelefone() { return telefone; }
+    public String getCpf() { return cpf; }
+
+    @Override
+    public String toString() {
+        return nome + " (Tel: " + telefone + ", CPF: " + cpf + ")";
+    }
+}
